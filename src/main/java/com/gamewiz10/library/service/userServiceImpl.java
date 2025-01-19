2311a28914fc4e6577f05ab91ad797da961c1438
gamewiz10 <joseph.bell.20@cnu.edu>
2025-01-19 01:28:52 -0500

    added pom

--- src/main/java/com/gamewiz10/library/service/userServiceImpl.java
+++ src/main/java/com/gamewiz10/library/service/userServiceImpl.java
@@ -1,4 +1,53 @@
 package com.gamewiz10.library.service;
 
-public class userServiceImpl {
+import com.gamewiz10.library.entity.User;
+import com.gamewiz10.library.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class userServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public userServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User createUser(User user) {
+        if (userRepository.existsByUsername(user.getUsername())){
+            throw new IllegalArgumentException("Username is already taken.");
+        }
+        if (userRepository.existsByEmail(user.getEmail())){
+            throw new IllegalArgumentException("Email is already in use.");
+        }
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+
+    @Override
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
 }
